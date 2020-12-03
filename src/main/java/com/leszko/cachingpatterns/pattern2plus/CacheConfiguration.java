ace710cc6b5ef5f5360b790b947fc1567423e9e9
Rafal Leszko <rafal@hazelcast.com>
2020-12-03 16:52:01 +0100

    Update to Hazelcast 4.1

--- src/main/java/com/leszko/cachingpatterns/pattern2plus/CacheConfiguration.java
+++ src/main/java/com/leszko/cachingpatterns/pattern2plus/CacheConfiguration.java
@@ -2,7 +2,6 @@ package com.leszko.cachingpatterns.pattern2plus;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.config.GroupConfig;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +15,7 @@ public class CacheConfiguration {
         clientConfig.getNetworkConfig().getCloudConfig()
             .setEnabled(true)
             .setDiscoveryToken("KSXFDTi5HXPJGR0wRAjLgKe45tvEEhd");
-        clientConfig.setGroupConfig(new GroupConfig("test-cluster", "b2f984b5dd3314"));
+        clientConfig.setClusterName("test-cluster");
 
         return new HazelcastCacheManager(
             HazelcastClient.newHazelcastClient(clientConfig));
